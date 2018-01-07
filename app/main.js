9becba70b01811c7bfe654ca043abf327d78f69c
So <1979510177@qq.com>
2018-01-07 12:30:38 +0800

    test

--- app/main.js
+++ app/main.js
@@ -1,10 +1,10 @@
 // var greeter = require('./Greeter.js');
 // document.getElementById('root').appendChild(greeter());
-import React from 'react';
-import {render} from 'react-dom';
-import Greeter from './Greeter';
+import React from 'react'
+import {render} from 'react-dom'
+import Greeter from './Greeter'
 import './main.less'
-
-render(<Greeter />, document.getElementById('root'));
+console.log(11)
+render(<Greeter />, document.getElementById('root'))
 
 // window.gg=Greeter
